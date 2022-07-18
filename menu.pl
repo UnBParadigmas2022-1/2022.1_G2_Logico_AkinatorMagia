c478a76b1538412278748b16cbec8e70598d562b
Giovanna Borges Bottino <31159235+giovannabbottino@users.noreply.github.com>
2022-07-18 15:53:57 -0300

    Create menu.pl

---
+++ menu.pl
@@ -0,0 +1,15 @@
+:- initialization(menu).
+menu:-  repeat,
+	write('=== MENU ==='), nl,
+	write('1. Truque de magica'), nl,
+	write('2. Akinator'), nl,
+	write('0. Sair'), nl,
+	read(X),
+	option(X),
+	X==0,
+	!.
+
+option(0):- !.
+option(1):- write('Abrindo truque de mágica...'), nl, !.
+option(2):- write('Abrindo akinator...'), nl, !.
+option(_):- write('Não é uma opção válida'), nl, !.
