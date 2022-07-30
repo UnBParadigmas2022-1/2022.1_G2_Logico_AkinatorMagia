9d1238db52324cb820fd5052ce9357d1e621de96
Giuulob89 <giulialobo89@gmail.com>
2022-07-30 18:52:44 -0300

    Finaliza truque na interface e estiliza as janelas
    Co-authored-by: Murilo Gomes <murilo.goomes@hotmail.com>

--- menu.pl
+++ menu.pl
@@ -1,4 +1,5 @@
 :- initialization(menu).
+:- use_module(library(pce_style_item)).
 :- use_module(library(pce)).
 
 :- pce_begin_class(fixed_size_button, button).
@@ -10,23 +11,27 @@ compute(_) :-> true.
 menu:-  repeat,
 	new(W, dialog('Menu')),
 	send(W, size, size(1000, 800)),
-	new(T, text('Escolha seu jogo')),
-	new(F, font(screen, bold, 35)),
+	new(T, text('    Escolha seu jogo')),
+	new(F, font(times, bold, 35)),
 	send(T, font(F)),
+	send(W, gap, size(335, 200)),
 	send(W, append, new(BTS, dialog_group(buttons, group))),
-	send(BTS, gap, size(0, 30)),
+	send(T, colour('#D9CF68')),
+	send(BTS, colour(blue)),
+	send(W, background, '#7D97D9'),
+	send(BTS, gap, size(185, 40)),
 	send(BTS, append, T),
 	send(BTS, append, new(B1, fixed_size_button('Truque de mágica', message(@prolog, option, 1))), below),
 	send(BTS, append, new(B2, fixed_size_button('Akinator', message(@prolog, option, 2))), below),
 	send(BTS, append, new(B3, fixed_size_button('Sair', message(W, destroy))), below),
 	send_list([B1,B2,B3], size(size(320,50))),
-	% send(BTS, alignment(center)),
 	send(BTS, layout_dialog),
+	send(W, layout),
     send(W, open),
 	!.
 
 option(0):- !.
-option(1):- write('Abrindo truque de mágica...'), nl,
+option(1):-
     consult(truque/magica), !.
 option(2):- write('Abrindo akinator...'), nl, !.
 option(_):- write('Não é uma opção válida'), nl, !.
--- truque/magica.pl
+++ truque/magica.pl
@@ -11,50 +11,58 @@ magica(X, [H|T]) :-
 magica(X, []).
 
 table(W, I, [H|T]) :- 
-    format(atom(S), '~w', (I=H)),
-    new(F1, font(screen, roman, 15)),
+    format(atom(S), '                    ~w', (I=H)),
+    new(F1, font(screen, roman, 20)),
     new(T1, text(S)),
 	send(T1, font(F1)),
+    send(T1,colour('#F5F5F5')),
     send(W, append, T1),
     R is I+1,
     table(W, R, T).
 table(_, _, []).
 
 play(W) :- 
-    writeln("Vou advinhar sua carta!"),
     game(W, ['K♠', 'Q♣', 'J♦', 'A♥', '2♠', '3♣', '4♦', '5♥', '6♠', '7♣', '8♦', '9♥'], []), !.
 
-verifica(Escolha) :- 
+verifica(Escolha, W) :- 
     length(Escolha, LE), LE > 1,
     findall(X, magica(X, Escolha), L),
     length(L, N), N == 1,
     magica(Z, Escolha),
-    write('Sua carta é a '), write(Z), write('!'), nl.
+    finaliza_jogo(W, Z).
 
 cria_tela :-
     new(W, dialog('Menu')),
     
-	send(W, size, size(1000, 800)),
+	send(W, size, size(900, 500)),
     send(W, open),
     play(W).
 
 game(W, Baralho, Escolhas) :- repeat,
 	new(T, text('Escolha uma carta e fale em qual conjunto ela está')),
-	new(F, font(screen, bold, 15)),
+	new(F, font(screen, bold, 25)),
 	send(T, font(F)),
+    send(T, colour('#cd081c')),
     separa(Baralho, Baralhos),
     nb_setval(baralhos, Baralhos),
     nb_setval(escolhas, Escolhas),
+    send(W, gap, size(90, 70)),
     send(W, append, T),
-    table(W, 1, Baralhos),
-	
-	send(W, append, new(B1, fixed_size_button('Deque 1',
+    send(W, append, new(DG1, dialog_group(buttons, group)), below),
+    send(DG1, gap, size(0, 60)),
+    table(DG1, 1, Baralhos),
+    send(DG1, layout_dialog),
+    send(W, append, new(DG2, dialog_group(buttons, group)), below),
+    send(DG2, gap, size(230, 30)),
+    send(DG2, append, text('')),
+	send(DG2, append, new(B1, fixed_size_button('Deque 1',
         message(@prolog, escolhe, W, 1))), below),
-	send(W, append, new(B2, fixed_size_button('Deque 2',
+	send(DG2, append, new(B2, fixed_size_button('Deque 2',
         message(@prolog, escolhe, W, 2))), right),
-    send_list([B1,B2], size(size(220,30))),
+    send_list([B1,B2], size(size(250,40))),
+    send(DG2, layout_dialog),
     send(W, layout),
-
+    send(W, background, '#3d5047'),
     !.
 
 separa(Baralho, Baralhos) :- 
@@ -75,7 +83,23 @@ escolhe(W, Escolha) :-
     nth0(Index, Baralhos, Deque),
     append([Deque], Escolhas, NovasEscolhas),
     (
-        verifica(NovasEscolhas); % verifica se ele já sabe a carta
+        verifica(NovasEscolhas, W); % verifica se ele já sabe a carta
         game(W, ['K♠', 'Q♣', 'J♦', 'A♥', '2♠', '3♣', '4♦', '5♥', '6♠', '7♣', '8♦', '9♥'], NovasEscolhas)
     );
     !.
+
+finaliza_jogo(W, Carta) :-
+    format(atom(S), 'Sua carta é a ~w!', Carta),
+    new(SI, text(S)),
+    new(TI, text('Fim do Jogo!')),
+	new(FI, font(screen, bold, 35)),
+	send(TI, font(FI)),
+    send(SI, font(FI)),
+    send(TI,colour('#cd081c')),
+    send(SI,colour('#cd081c')),
+    send(W, append(TI)),
+    send(W, append, SI, below),
+    send(W, gap, size(100, 30)),
+    send(W, append, new(B3, fixed_size_button('Sair', message(W, destroy))), below),
+    send(B3, size(size(350,40))),
+    send(W, layout).
