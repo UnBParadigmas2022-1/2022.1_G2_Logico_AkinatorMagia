e9ec2f0b9bfcca63fe9afe3f373d1722a255b40a
Henrique Melo <hacmelo@gmail.com>
2022-08-01 10:21:13 -0300

    integracao perguntasCentroOeste

--- akinator/akinator.pl
+++ akinator/akinator.pl
@@ -229,41 +229,44 @@ perguntasNordeste :-
 
     anunciaResultado.
 
-perguntasCentroOeste :- write("É do centro oeste!"),
+perguntasCentroOeste :-
 % +5M de habitantes, densidadePopulacional, Divisa com + de 3 estados
 
     retractall(estado(_, nordeste, _, _, _, _, _, _, _, _)),
     retractall(estado(_, norte, _, _, _, _, _, _, _, _)),
     retractall(estado(_, sudeste, _, _, _, _, _, _, _, _)),
     retractall(estado(_, sul, _, _, _, _, _, _, _, _)),
-    forall(estado(N,R,L,DOR,DP4,DP5e10,DE,MH,A,NC), writeln(estado(N,R,L,DOR,DP4,DP5e10,DE,MH,A,NC))),
+    %forall(estado(N,R,L,DOR,DP4,DP5e10,DE,MH,A,NC), writeln(estado(N,R,L,DOR,DP4,DP5e10,DE,MH,A,NC))),
 
-    write("Seu estado tem mais de 5 milhões de habitantes?"),
-    read(X),
-    ifThenElse(resposta(sim,X),
-    retractall(estado(_, _, _, _, _, _, _, no, _, _)),
-    retractall(estado(_, _, _, _, _, _, _, yes, _, _))
+    nb_setval(pergunta, 'Seu estado tem mais de 5 milhões de habitantes?'),
+    mostra_pergunta(W),
+    nb_getval(resposta, Resposta),
+    (Resposta=2 ->
+        retractall(estado(_, _, _, _, _, _, _, yes, _, _));
+        retractall(estado(_, _, _, _, _, _, _, no, _, _))
     ),
-    forall(estado(N,R,L,DOR,DP4,DP5e10,DE,MH,A,NC), writeln(estado(N,R,L,DOR,DP4,DP5e10,DE,MH,A,NC))),
+    %forall(estado(N,R,L,DOR,DP4,DP5e10,DE,MH,A,NC), writeln(estado(N,R,L,DOR,DP4,DP5e10,DE,MH,A,NC))),
 
-    write("A densidade populacional do seu estado é menor que 4?"),
-    read(Y),
-    ifThenElse(resposta(sim,Y),
-    retractall(estado(_, _, _, _, no, _, _, _, _, _)),
-    retractall(estado(_, _, _, _, yes, _, _, _, _, _))
+    nb_setval(pergunta, 'A densidade populacional do seu estado é menor que 4?'),
+    mostra_pergunta(W),
+    nb_getval(resposta, Resposta),
+    (Resposta=2 ->
+        retractall(estado(_, _, _, _, yes, _, _, _, _, _));
+        retractall(estado(_, _, _, _, no, _, _, _, _, _))
     ),
-    forall(estado(N,R,L,DOR,DP4,DP5e10,DE,MH,A,NC), writeln(estado(N,R,L,DOR,DP4,DP5e10,DE,MH,A,NC))),
+    %forall(estado(N,R,L,DOR,DP4,DP5e10,DE,MH,A,NC), writeln(estado(N,R,L,DOR,DP4,DP5e10,DE,MH,A,NC))),
 
-    write("O seu estado faz divisa com mais de três estados?"),
-    read(Z),
-    ifThenElse(resposta(sim,Z),
-    retractall(estado(_, _, _, _, _, _, no, _, _, _)),
-    retractall(estado(_, _, _, _, _, _, yes, _, _, _))
+    nb_setval(pergunta, 'O seu estado faz divisa com mais de três estados?'),
+    mostra_pergunta(W),
+    nb_getval(resposta, Resposta),
+    (Resposta=2 ->
+        retractall(estado(_, _, _, _, _, _, yes, _, _, _));
+        retractall(estado(_, _, _, _, _, _, no, _, _, _))
     ),
-    forall(estado(N,R,L,DOR,DP4,DP5e10,DE,MH,A,NC), writeln(estado(N,R,L,DOR,DP4,DP5e10,DE,MH,A,NC))),
+    %forall(estado(N,R,L,DOR,DP4,DP5e10,DE,MH,A,NC), writeln(estado(N,R,L,DOR,DP4,DP5e10,DE,MH,A,NC))),
 
     anunciaResultado.
-  
+
 
 perguntasSudeste :- 
 % litoral, +5 mi de habitantes, divisa com +3 estados
